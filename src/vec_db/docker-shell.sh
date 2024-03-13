d6c998af31c29c44e98738f93847f6efefda8ef4
Bailey Bai <yuqinbai@g.harvard.edu>
2024-03-13 14:09:47 +0100

    update vec_db container volume

--- src/vec_db/docker-shell.sh
+++ src/vec_db/docker-shell.sh
@@ -9,8 +9,8 @@ docker network inspect vec-db-network >/dev/null 2>&1 || docker network create v
 docker build -t vec-db --platform=linux/arm64 -f Dockerfile .
 
 # Ensure volume for persistent storage exists
-docker volume inspect vec-db-storage >/dev/null 2>&1 || docker volume create vec-db-storage
+# docker volume inspect metadata >/dev/null 2>&1 || docker volume create metadata
 
 # Run the container with the volume
-# docker run -d --name vec-db-container --network vec-db-network -v vec-db-storage:/app/metadata vec-db
+# docker run -d --name vec-db-container --network vec-db-network -v metadata:/app/metadata vec-db
 docker-compose run --rm --service-ports vec-db
