c99e625d095ec06d01a5a03d91c4d9f26ef379f3
Li Yao <li_yao@fas.harvard.edu>
2024-03-13 14:09:59 +0100

    update

--- src/retrieval/retrieval.py
+++ src/retrieval/retrieval.py
@@ -3,26 +3,11 @@ import openai
 import sys
 import json
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.vectorstores import FAISS
 
 
 from google.cloud import storage
-
-
-client = storage.Client()
-bucket = client.bucket('team-birth-2024')
-
-blob_df = bucket.blob("data_wa/vec_db/wlasl_test_new.csv")
-train_df = pd.read_csv(blob_df.open())
-
-print("\n\n... LOAD SIGN TO PREDICTION INDEX MAP FROM JSON FILE ...\n")
-# Read character to prediction index
-blob_json = bucket.blob("data/WLASL-data/sign_to_prediction_index_map.json")
-with blob_json.open("r") as f:
-    json_file = json.loads(f.read())
 
 
 
@@ -38,30 +23,50 @@ def openai_setup(secret_path: str):
 
 
 
+client = storage.Client()
+bucket = client.bucket('data_wa')
+blobs = bucket.list_blobs()
+for b in blobs:
+    print(b.name)
+
+# embeddings = HuggingFaceInstructEmbeddings()
+# blob_vectordb = bucket.blob("data_wa/vec_db/index.faiss")
+# vectordb = FAISS.load_local(blob_vectordb, embeddings, allow_dangerous_deserialization=True)
+
+for f in ['index.faiss', 'index.pkl']:
+    blob = bucket.blob(f'vec_db/{f}')
+    print(blob)
+    blob.download_to_filename(os.path.join('vec_db', f))
+db = FAISS.load_local('vec_db', embeddings=HuggingFaceInstructEmbeddings())
+
+
+retriever=db.as_retriever()
+
 openai_setup('../secrets/openai_secret.json')
 
-loader = CSVLoader(file_path='./teambirth_surveys/Washington.csv')
-docs = loader.load()
+# loader = CSVLoader(file_path='./teambirth_surveys/Washington.csv')
+# docs = loader.load()
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 500,
-    chunk_overlap = 150,
-    separators=["\n\n", "\n", "(?<=\. )", " ", ""]
-)
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size = 500,
+#     chunk_overlap = 150,
+#     separators=["\n\n", "\n", "(?<=\. )", " ", ""]
+# )
 
-splits = text_splitter.split_documents(docs)
-embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-vectordb = FAISS.from_documents(documents=splits, embedding=embedding)
+# splits = text_splitter.split_documents(docs)
+# embedding = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+# vectordb = FAISS.from_documents(documents=splits, embedding=embedding)
 
 #db.save_local("faiss_index")
 #vectordb = FAISS.load_local("faiss_index", embeddings)
 
-retriever=vectordb.as_retriever()
+
 
 if __name__ == "__main__":
     
     question = input("Please enter your question: ")
     docs = retriever.invoke(question)
     print(docs)  
+
 
     
